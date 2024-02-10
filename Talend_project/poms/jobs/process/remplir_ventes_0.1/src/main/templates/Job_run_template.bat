c4bb1c7171011a4e4922238408a3330f600966a7
Romain Andres <romain.andres@etu.unicaen.fr>
2024-02-10 22:10:14 +0100

    add files

---
+++ Talend_project/poms/jobs/process/remplir_ventes_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/log4j-1.2-api-2.12.1.jar;../lib/commons-collections-3.2.2.jar;../lib/sqlitejdbc-v056.jar;../lib/jboss-serialization.jar;../lib/advancedPersistentLookupLib-1.2.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;../lib/trove.jar;../lib/crypto-utils.jar;remplir_ventes_0_1.jar; filrouge.remplir_ventes_0_1.remplir_ventes  %*
